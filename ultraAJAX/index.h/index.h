110047b860c45009826e74d43c1655f23cb077ca
Barbra Gitonga <gitongabarbra@gmail.com>
2022-11-19 19:19:31 +0300

    change in index file

--- ultraAJAX/index.h/index.h
+++ ultraAJAX/index.h/index.h
@@ -1,21 +1,42 @@
 const char MAIN_page[] PROGMEM = R"=====(
 <!DOCTYPE html>
 <html>
-  setInterval(function() {
-  getData();
-  }, 1000);
+<style>
+.card{
+    max-width: 400px;
+     min-height: 250px;
+     background: #02b875;
+     padding: 30px;
+     box-sizing: border-box;
+     color: #FFF;
+     margin:20px;
+     box-shadow: 0px 2px 18px -4px rgba(0,0,0,0.75);
+}
+</style>
+<body>
 
-  function getData() {
-    var GetDataRequest = new XMLHttpRequest();
-    GetDataRequest.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) 
-      {
-        document.GetelementByID("Count").innerHTML = 
-        this.responseText;  
-      }
-    };
-    GetDataRequest.open("GET", "readCount", true);
-    GetDataRequest.send();  
-  }
+<div class="card">
+  <h1>Sensor Value:<span id="Num"></span></h1><br>
+</div>
+<script>
+
+setInterval(function() {
+  // Call a function repetatively with 2 Second interval
+  getData();
+}, 2000); //2000mSeconds update rate
+
+function getData() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("Num").innerHTML =
+      this.responseText;
+    }
+  };
+  xhttp.open("GET", "readADC", true);
+  xhttp.send();
+}
+</script>
+</body>
 </html>
 )=====";
--- ultraAJAX/ultraAJAX.ino
+++ ultraAJAX/ultraAJAX.ino
@@ -3,8 +3,8 @@
 #include <WebServer.h>
 #include "index.h"
 
-const char *ssid = "Nezuko";
-const char *password = "NezukoChan542";
+const char *ssid = "";
+const char *password = "";
 
 WebServer server(80);
 
@@ -94,78 +94,7 @@ void setup() {
 
 void loop() {
   
-  // // Clears the trigPin
-  // digitalWrite(trigPin, LOW);
-  // delayMicroseconds(2);
-  // // Sets the trigPin on HIGH state for 10 micro seconds
-  // digitalWrite(trigPin, HIGH);
-  // delayMicroseconds(10);
-  // digitalWrite(trigPin, LOW);
-  // // Reads the echoPin, returns the sound wave travel time in microseconds
-  // duration = pulseIn(echoPin, HIGH);
-  // // Calculating the distance
-  // distance = duration * 0.034 / 2;
-  // // Prints the distance on the Serial Monitor
-  // Serial.print("Distance: ");
-  // Serial.println(distance);
-  // delay(200);
-
-
-  // if (distance < 10) {
-  //   Count += 1;
-  // } else {  // distance <=10
-  //   Count;
-  // }
-  // Serial.print("count: ");
-  // Serial.println(Count);
-
-  WiFiClient client;   // listen for incoming clients
-  
   server.handleClient();
   delay(1);
-//  if (client) {                             // if you get a client,
-//    Serial.println("New Client.");           // print a message out the serial port
-//    String currentLine = "";                // make a String to hold incoming data from the client
-//    while (client.connected()) {            // loop while the client's connected
-//      if (client.available()) {             // if there's bytes to read from the client,
-//        char c = client.read();             // read a byte, then
-//        Serial.write(c);                    // print it out the serial monitor
-//        if (c == '\n') {                    // if the byte is a newline character
-//
-//          // if the current line is blank, you got two newline characters in a row.
-//          // that's the end of the client HTTP request, so send a response:
-//          if (currentLine.length() == 0) {
-//            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-//            // and a content-type so the client knows what's coming, then a blank line:
-//            client.println("HTTP/1.1 200 OK");
-//            client.println("Content-type:text/html");
-//            client.println();
-//
-//            // the content of the HTTP response follows the header:
-//            client.print("Click <a href=\"/H\">here</a> to see the COUNT.<br>");
-//            // The HTTP response ends with another blank line:
-//            client.println();
-//            // break out of the while loop:
-//            break;
-//          } else {    // if you got a newline, then clear currentLine:
-//            currentLine = "";
-//          }
-//        } else if (c != '\r') {  // if you got anything else but a carriage return character,
-//          currentLine += c;      // add it to the end of the currentLine
-//        }
-//
-//        // Check to see if the client request was "GET /H" 
-//        if (currentLine.endsWith("GET /H")) 
-//        {
-//          client.println ("Count: ");
-//          client.println(COUNT);
-//        }
-//        
-//      }
-//    }
-//    // close the connection:
-//    client.stop();
-//    Serial.println("Client Disconnected.");
-//    
-//  }
+
 }
